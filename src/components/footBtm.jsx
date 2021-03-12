c219fd86945e833a695bb7238dc02434fdae86a1
srdothunter <srdothunter@gmail.com>
2021-03-12 23:45:29 +0800

    fix bug of eslint

--- src/components/footBtm.jsx
+++ src/components/footBtm.jsx
@@ -5,11 +5,11 @@ const FootBtm = ()=>{
         <div className='footer'>
             <div className='container'>
                 <div className='col-12'>
-                    <a href="https://www.subdao.network/" target="_blank">Website</a>
-                    <a href="https://drive.google.com/file/d/1_FBX68rF-mOZ7AoaF7W_tWuRGW-tJ_Kc/view?usp=sharing" target="_blank">Light Paper</a>
-                    <a href="https://github.com/SubDAO-Network" target="_blank">Github</a>
-                    <a href="https://medium.com/@subdao" target="_blank">Medium</a>
-                    <a href="https://twitter.com/subdao_network" target="_blank">Twitter</a>
+                    <a href="https://www.subdao.network/" rel="noopener noreferrer" target="_blank">Website</a>
+                    <a href="https://drive.google.com/file/d/1_FBX68rF-mOZ7AoaF7W_tWuRGW-tJ_Kc/view?usp=sharing" rel="noopener noreferrer"target="_blank">Light Paper</a>
+                    <a href="https://github.com/SubDAO-Network" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a href="https://medium.com/@subdao" target="_blank" rel="noopener noreferrer">Medium</a>
+                    <a href="https://twitter.com/subdao_network" target="_blank" rel="noopener noreferrer">Twitter</a>
                 </div>
             </div>
 
