718593e7e5f5421b1bfd462998277eb0119d5fe9
srdothunter <wendy8449@gmail.com>
2021-06-07 15:39:36 +0800

    update the config of main contract &websocket

--- public/config.js
+++ public/config.js
@@ -1,9 +1,6 @@
 window.mainAddress = {
     // main: "5G53tdfgCrCBr4qs6QPMw7HR7EoVoNB4e4mbxHy5QrYKArZi",
-    main: "5GodmyTkVKacMjhfmYHS2FgRj5SHLnwJZhobhuA69Q99EPqd",
-    rpc_server: "ws://39.101.70.206:9944"
+    main: "5F1UfbQpeTeAs9RrTUDEtVpZAX3Y3houLKuGTrsHXADtiVwC",
+    rpc_server: "wss://alpha.subdao.org"
 
-    // 3.0
-    // main: "5G3sxWeToZx2bzJDt4Lf5jGQmQvQxHDErHrJ4K6RwFg7Kmck",
-    // rpc_server: "ws://39.101.70.206:39944"
 };
