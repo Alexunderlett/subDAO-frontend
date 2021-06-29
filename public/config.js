30ee66dfaf76e31129f150de1cfbe69147012747
srdothunter <wendy8449@gmail.com>
2021-06-29 23:13:42 +0800

    update config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,6 @@
 window.mainAddress = {
-    // main: "5G53tdfgCrCBr4qs6QPMw7HR7EoVoNB4e4mbxHy5QrYKArZi",
-
-    // main: "5GodmyTkVKacMjhfmYHS2FgRj5SHLnwJZhobhuA69Q99EPqd",
-    // rpc_server: "ws://39.101.70.206:9944"
-
     // 3.0
-    main: "5GiM7G3gWLZosby8oAfjFK7es8c1qjMPG32rRGAZqeeLCEqU",
-    // main: "5ERyuE3zDRQZzNd5n6NSJKaFuojAUQu7gH7QEV2X4zDHCX2q",
-    rpc_server: "ws://39.101.70.206:39944"
+    main: "5DZMXtTDHVXgwDuH7zbiFFcQ9KAkU9UeSPXXBkrnPoZ9k95S",
+    rpc_server: "wss://alpha.subdao.org"
 
 };
