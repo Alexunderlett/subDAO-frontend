cb8cfc2f2a7567777a2a20fd4cb3f0e8da8c955c
srdothunter <wendy8449@gmail.com>
2021-07-15 22:49:32 +0800

    update new main address

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.mainAddress = {
     // 3.0
-    main: "5CEtZvkk9paJorCaoud1YkaUco15gFF5EA7WUroDbHDisrzw" ,
+    main: "5CpdUzCos1b6yzvXw4yW3PB986eEDJZ4QBGUkee5jxKUuzfd" ,
     rpc_server: "wss://alpha.subdao.org"
 
 };
