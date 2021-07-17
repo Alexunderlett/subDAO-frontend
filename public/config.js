084b9270416d325bdc200f4f67c5c6ac81dad54c
HA <>
2021-07-18 00:57:49 +0800

    chore: update contract

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.mainAddress = {
     // 3.0
-    main: "5CpdUzCos1b6yzvXw4yW3PB986eEDJZ4QBGUkee5jxKUuzfd" ,
+    main: "5Cb927g9D8MaGq7TbNDB6oAeEGxHZPP5YxvkZfi1GBFxrDhy" ,
     rpc_server: "wss://alpha.subdao.org"
 
 };
